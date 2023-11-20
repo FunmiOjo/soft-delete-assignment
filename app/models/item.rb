a8c68a4754c8f35b7b2e7cf912321f6308db7c1a
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2023-11-20 17:50:30 -0500

    Restore implementation and test

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
     def soft_delete
         self.update!(:deleted_at => Time.current)
     end
+
+    def restore
+        self.update!(:deleted_at => nil)
+    end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,10 +4,20 @@ RSpec.describe Item do
     describe 'soft_delete' do
         it 'updates deleted_at attribute of item to current time' do
             item = Item.create!(:name => 'item_1')
-            now = Time.now
+            now = Time.current
             item.soft_delete
             deleted_item = Item.find(item.id)
             expect(deleted_item.deleted_at).to be >= now
+        end
+    end
+
+    describe 'restore' do
+        it 'updates deleted_at attribute to nil' do
+            item = Item.create!(:name => 'item_2')
+            item.soft_delete
+            item.restore
+            restored_item = Item.find(item.id)
+            expect(restored_item.deleted_at).to eq(nil)
         end
     end
 end
